76a6177b4596a435e59b5b54af6ca1c0487b8f39
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-20 19:17:25 +0300

    feat(pages/JobsPage): lay out jobs page

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -1,8 +1,20 @@
 import React, { FunctionComponent } from 'react';
 
+import { JobsFilter } from 'common/components';
+
+import styles from './JobsPage.module.scss';
+
 const JobsPage: FunctionComponent = () => {
   return (
-    <p>Jobs page</p>
+    <div className={styles.wrapper}>
+      <JobsFilter />
+      <div className={styles.main}>
+        <input type="text" placeholder="Введите название вакансии" />
+        <div className={styles.jobs}>
+          Jobs found
+        </div>
+      </div>
+    </div>
   );
 };
 
