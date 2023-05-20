64d37a8206d19131f09289bb4e9c6eb3fb76ccde
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-21 01:38:12 +0300

    feat(data/hooks): implement useIndustries hook

---
+++ src/data/hooks/useIndustries.ts
@@ -0,0 +1,21 @@
+import useSWR from 'swr';
+
+import { Industry } from '../model';
+import { buildURL, fetcher } from '../utils';
+
+export interface UseIndustriesResult {
+  industries: Industry[];
+  isLoading: boolean;
+  isError: boolean;
+}
+
+export const useIndustries = (): UseIndustriesResult => {
+  const url = buildURL('/catalogues');
+  const { data, isLoading, error} = useSWR<Industry[]>(url, fetcher);
+
+  return {
+    industries: data,
+    isLoading,
+    isError: Boolean(error),
+  };
+};
