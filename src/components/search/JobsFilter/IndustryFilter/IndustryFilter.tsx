c49a1c68d214027bc1fab16ca78b40db8a4457c9
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-21 01:39:20 +0300

    feat(components/search): feed IndustryFilter

--- src/components/search/JobsFilter/IndustryFilter/IndustryFilter.tsx
+++ src/components/search/JobsFilter/IndustryFilter/IndustryFilter.tsx
@@ -1,18 +1,28 @@
-import { FunctionComponent } from 'react';
-import { Select } from '@mantine/core';
+import { FunctionComponent, useMemo } from 'react';
+import { Select, SelectItem } from '@mantine/core';
 
 import styles from './IndustryFilter.module.scss';
 import { ShevronIcon } from '../ShevronIcon';
+import { useIndustries } from 'data/hooks';
+import { Industry } from 'data/model';
 
 export const IndustryFilter: FunctionComponent = () => {
+  const { industries } = useIndustries();
+
+  const options: SelectItem[] = useMemo(() => {
+    return industries?.map((industry: Industry): SelectItem => ({
+      value: industry.key.toString(),
+      label: industry.title,
+    })) || [
+      { value: '', label: '(Нет данных)', disabled: true },
+    ];
+  }, [industries]);
+
   return (
     <Select
       label="Отрасль"
       placeholder="Выберите отрасль"
-      data={[
-        { value: '1', label: 'IT' },
-        { value: '2', label: 'Финансы' },
-      ]}
+      data={options}
       rightSection={<ShevronIcon />}
       styles={{
         rightSection: {
