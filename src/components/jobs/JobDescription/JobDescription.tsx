91e544779b52e752a37e48092efb527ecae4fb20
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-22 22:39:47 +0300

    feat(components/jobs): implement JobDescription component

---
+++ src/components/jobs/JobDescription/JobDescription.tsx
@@ -0,0 +1,18 @@
+import { Job } from 'data/model';
+import styles from './JobDescription.module.scss';
+import { FunctionComponent } from 'react';
+
+interface JobDescriptionProps {
+  job: Job;
+}
+
+export const JobDescription: FunctionComponent<JobDescriptionProps> = ({ job }) => {
+  return (
+    <div
+      className={styles.wrapper}
+      dangerouslySetInnerHTML={{
+        __html: job.vacancy_rich_text,
+      }}
+    />
+  );
+};
