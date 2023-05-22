efc88e2f777ab015678b92e45e5b69a726f74d62
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-23 01:57:24 +0300

    feat(pages/jobs): add job cards

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -1,22 +1,38 @@
 import React, { FunctionComponent, useReducer } from 'react';
 
 import { JobsFilter, SearchBar } from 'components/search';
+import { JobCard, JobCardVariant } from 'components/jobs';
 import { searchReducer, initialSearchState } from 'reducers/search';
+import { Job } from 'data/model';
+import { useJobs } from 'data/hooks';
 
 import styles from './JobsPage.module.scss';
 
 const JobsPage: FunctionComponent = () => {
   const [searchState, dispatch] = useReducer(searchReducer, initialSearchState);
 
+  const { industryId, salaryRange, jobQuery } = searchState;
+
+  const { jobsList } = useJobs({
+    catalogues: industryId,
+    payment_from: salaryRange.from,
+    payment_to: salaryRange.to,
+    keyword: jobQuery,
+    // TODO: implement pagination
+    page: 1,
+  });
+
   return (
     <div className={styles.wrapper}>
-      <JobsFilter
-        state={{
-          industryId: searchState.industryId,
-          salaryRange: searchState.salaryRange,
-        }}
-        dispatch={dispatch}
-      />
+      <div className={styles.aside}>
+        <JobsFilter
+          state={{
+            industryId: searchState.industryId,
+            salaryRange: searchState.salaryRange,
+          }}
+          dispatch={dispatch}
+        />
+      </div>
       <div className={styles.main}>
         <SearchBar
           state={{
@@ -25,7 +41,9 @@ const JobsPage: FunctionComponent = () => {
           dispatch={dispatch}
         />
         <div className={styles.jobs}>
-          {JSON.stringify(searchState)}
+          {jobsList?.objects.map((job: Job): JSX.Element => (
+            <JobCard key={job.id} job={job} variant={JobCardVariant.Link} />
+          ))}
         </div>
       </div>
     </div>
