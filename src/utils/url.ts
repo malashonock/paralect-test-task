60bb896b40f26af8138f63bacc2e273d23a48519
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-23 05:37:20 +0300

    fix(utils/url): fix array params presentation

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,20 +3,35 @@ export const buildURL = (endpoint: string, searchParams?: Record<string, any>): 
   return process.env.NEXT_PUBLIC_API_BASEURL + endpoint + queryString;
 };
 
+const stringify = (value: any): string => {
+  return typeof value === 'string' ? value : value.toString();
+};
+
 export const buildQueryString = (searchParams: Record<string, any>): string => {
   const stringifiedParams: Record<string, string> = Object.fromEntries(
-    Object.entries(searchParams).map(([key, value]: [string, any]): [string, string] | undefined => {
+    Object.entries(searchParams).reduce((prev: Array<[string, string]>, [key, value]: [string, any]): Array<[string, string]> => {
       if (!value) {
-        return undefined;
+        return prev;
       }
 
-      const stringifiedValue: string = typeof value === 'string' ? value : value.toString();
+      if (Array.isArray(value)) {
+        return [
+          ...prev, 
+          ...value.map((value, index: number): [string, string] => [
+            key + `[${index}]`,
+            stringify(value),
+          ]),
+        ];
+      }
 
       return [
-        key,
-        stringifiedValue,
-      ]
-    }).filter((entry: [string, string] | undefined): boolean => Array.isArray(entry)),
+        ...prev, 
+        [
+          key,
+          stringify(value),
+        ]
+      ];
+    }, []),
   );
 
   const queryString: string = new URLSearchParams(stringifiedParams).toString();
