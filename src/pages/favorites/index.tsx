e89b5cfdf10d1c093b9819c06106b2dba8f521fd
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-23 05:37:35 +0300

    feat(pages): implement favorites page

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,8 +1,33 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+
+import { JobCard, JobCardVariant } from 'components/jobs';
+import { Job } from 'data/model';
+import { useJobs } from 'data/hooks';
+import { selectSavedJobIds } from 'store';
+
+import styles from './FavoritesPage.module.scss';
 
 const FavoritesPage: FunctionComponent = () => {
+  const savedJobIds = useSelector(selectSavedJobIds);
+
+  const { jobsList } = useJobs({
+    ids: savedJobIds,
+    // TODO: implement pagination
+    page: 0,
+  });
+
   return (
-    <p>Favorites page</p>
+    <div className={styles.wrapper}>
+      {jobsList?.objects.map((job: Job): JSX.Element => (
+        <JobCard
+          key={job.id}
+          job={job}
+          variant={JobCardVariant.Link}
+          isFavorite
+        />
+      ))}
+    </div>
   );
 };
 
