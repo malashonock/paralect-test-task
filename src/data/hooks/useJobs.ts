9109586ba627f6bacf763eb67c3b51280090080a
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-23 17:41:44 +0300

    feat(utils/privateFetcher): add retry on auth errors

--- src/data/hooks/useJobs.ts
+++ src/data/hooks/useJobs.ts
@@ -13,10 +13,7 @@ export interface UseJobsResult {
 export const useJobs = (searchParams?: JobSearchParams): UseJobsResult => {
   const url = buildURL('/vacancies/', searchParams);
 
-  // TODO: get from redux store
-  const accessToken: string = 'v3.r.137440105.3e8af7c9063f62c6e22e7948803c0633ccbb4e1e.7f1e761ccc7bb7c1b5833793cac1ee11b2a1a974';
-
-  const { data, isLoading, error } = useSWR<ListResponse<Job>>(url, privateFetcher(accessToken));
+  const { data, isLoading, error } = useSWR<ListResponse<Job>>(url, privateFetcher);
 
   return {
     jobsList: data,
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,4 +1,6 @@
-import { ApiResponse, ApiResponseError } from 'data/dto';
+import { ApiResponse, ApiResponseError, AuthError } from 'data/dto';
+import { store } from 'store';
+import { logIn, refreshTokenSet } from 'store/slices';
 
 export const publicFetcher = async <T>(url, overriddenOptions?: RequestInit): Promise<T> => {
   const rawResponse = await fetch(url, {
@@ -19,11 +21,44 @@ export const publicFetcher = async <T>(url, overriddenOptions?: RequestInit): Pr
   return parsedResponse as T;
 };
 
-export const privateFetcher = (accessToken: string) => async <T>(url): Promise<T> => {
-  return await publicFetcher<T>(url, {
-    headers: {
-      'x-api-app-id': process.env.NEXT_PUBLIC_CLIENT_SECRET || '',
-      'Authorization': `Bearer ${accessToken}`,
-    },
-  });
+export const privateFetcher = async <T>(url): Promise<T> => {
+  const { accessToken, refreshToken, tokenExpiryDate } = store.getState().auth;
+
+  if (!accessToken || !refreshToken || !tokenExpiryDate) {
+    store.dispatch(logIn());
+    return await privateFetcher(url); // Retry
+  }
+
+  if (new Date(tokenExpiryDate * 1000) < new Date()) {
+    await store.dispatch(refreshTokenSet());
+    return await privateFetcher(url); // Retry
+  }
+  
+  const headers: HeadersInit = {
+    'x-api-app-id': process.env.NEXT_PUBLIC_CLIENT_SECRET || '',
+    'Authorization': `Bearer ${accessToken}`,
+  };
+
+  try {
+    return await publicFetcher<T>(url, { headers });
+  } catch (error) {
+    if (error.error) {
+      const { code: errorCode, error: errorType } = error as AuthError;
+
+      if (
+        errorCode === 401 && errorType === 'invalid_token'
+        || errorCode === 404 && errorType === 'token_not_found'
+      ) {
+        store.dispatch(logIn());
+        return await privateFetcher(url); // Retry
+      }
+
+      if (errorCode === 410 && errorType === 'invalid_token') {
+        store.dispatch(refreshTokenSet());
+        return await privateFetcher(url); // Retry
+      }
+    }
+
+    throw error;
+  }
 };
