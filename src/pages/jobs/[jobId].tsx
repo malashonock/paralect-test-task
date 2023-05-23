0e5bc836ff3a58796256c642fd6b31b15228f665
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-23 18:10:03 +0300

    fix(pages/JobPage): enable favorite feature

--- src/pages/jobs/[jobId].tsx
+++ src/pages/jobs/[jobId].tsx
@@ -1,20 +1,27 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 
 import { JobCard, JobCardVariant, JobDescription } from 'components/jobs';
 import { useJob } from 'data/hooks';
+import { selectSavedJobIds } from 'store';
 
 import styles from './JobPage.module.scss';
-import { useRouter } from 'next/router';
 
 const JobPage: FunctionComponent = () => {
   const { query } = useRouter();
   const { jobId } = query;
 
   const { job } = useJob(jobId as string);
+  const savedJobIds = useSelector(selectSavedJobIds);
 
   return job ? (
     <div className={styles.wrapper}>
-      <JobCard job={job} variant={JobCardVariant.Opened} />
+      <JobCard
+        job={job}
+        variant={JobCardVariant.Opened}
+        isFavorite={savedJobIds.includes(job.id)}
+      />
       <JobDescription job={job} />
     </div>
   ) : null;
